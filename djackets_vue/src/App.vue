3e36d61822f031258758fd3b57023385ae306d27
Jonas <j.g.f.pettersson@gmail.com>
2021-04-28 21:40:53 +0200

    menu fix

--- djackets_vue/src/App.vue
+++ djackets_vue/src/App.vue
@@ -10,13 +10,19 @@
           aria-label="menu"
           aria-expanded="false"
           data-target="nabar-menu"
+          @click="showMobileMenu = !showMobileMenu"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+
+      <div
+        class="navbar-menu"
+        id="navbar-menu"
+        v-bind:class="{ 'is-active': showMobileMenu }"
+      >
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
@@ -25,7 +31,7 @@
               <router-link to="/log-in" class="button is-light"
                 >Log in</router-link
               >
-              <router-link to="/cart" class="button-is-success">
+              <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
                 <span>Cart</span>
               </router-link>
@@ -42,6 +48,16 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMobileMenu: false,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 @import "../node_modules/bulma";
