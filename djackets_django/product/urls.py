3db650d76053ee0c3b46cfdd1596812d957ea83b
Jonas <j.g.f.pettersson@gmail.com>
2021-04-30 19:58:49 +0200

    view a product

--- djackets_django/product/urls.py
+++ djackets_django/product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 
-from .views import LatestProductList
+from .views import LatestProductList, ProductDetail
 
 urlpatterns = [
-    path('latest-products/', LatestProductList.as_view())
+    path('latest-products/', LatestProductList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/',
+         ProductDetail.as_view())
 ]
