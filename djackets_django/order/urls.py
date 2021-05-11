d4b8d9a2f96d6253475bd1d80d50f6d1b1cfc5f0
Jonas <j.g.f.pettersson@gmail.com>
2021-05-11 13:18:47 +0200

    my orders

--- djackets_django/order/urls.py
+++ djackets_django/order/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import checkout
+from .views import checkout, OrdersList
 
 urlpatterns = [
-    path('checkout/', checkout)
+    path('checkout/', checkout),
+    path('orders/', OrdersList.as_view())
 ]
